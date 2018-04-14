d2da31fd1b47c1003998f62e1d791cb11ae7a849
www-data <smorton@mindgrub.com>
2018-04-14 20:40:23 +0000

    content ingestion prep

--- wp-content/themes/wtmd-radio/404.php
+++ wp-content/themes/wtmd-radio/404.php
@@ -1,15 +1,4 @@
-<div class="container">
-	<div class="row">
-		<article class="content">
-			<header class="page-header">
-				<h1 class="page-header__title">Page Not Found</h1>
-			</header>
-			
-			<div class="wysiwyg">
-				<p>Looks like the page you're looking for isn't here anymore.</p>
-			</div>
-		</article>
-
-		<?php mg_get_sidebar(); ?>
-	</div>
+<div class="content wysiwyg">
+	<h1>Page Not Found</h1>
+	<p>Looks like the page you're looking for isn't here anymore.</p>
 </div>
