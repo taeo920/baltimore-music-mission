ec34d9cc9166ab94d193cb6d01cd9447bd0cf124
Stephen Morton <smorton@mindgrub.com>
2018-04-18 22:35:37 -0400

    Added default and 404 page templates

--- wp-content/themes/wtmd-radio/404.php
+++ wp-content/themes/wtmd-radio/404.php
@@ -1,4 +1,15 @@
+<div class="hero hero--internal">
+	<div class="hero__container">
+		<div class="hero__search"><?php mg_get_template_part('partials', 'form-search'); ?></div>
+	</div>
+</div>
+
 <div class="content wysiwyg">
-	<h1>Page Not Found</h1>
-	<p>Looks like the page you're looking for isn't here anymore.</p>
+	<header class="page-header">
+		<h1 class="page-header__title">Page Not Found</h1>
+	</header>
+
+	<div class="wysiwyg">
+		<p>Looks like the page you're looking for isn't here anymore.</p>
+	</div>
 </div>
--- wp-content/themes/wtmd-radio/page.php
+++ wp-content/themes/wtmd-radio/page.php
@@ -1,17 +1,17 @@
 <?php the_post(); ?>
 
-<div class="container">
-	<div class="row">
-		<article class="content">
-			<header class="page-header">
-				<h1 class="page-header__title"><?php the_title(); ?></h1>
-			</header>
-			
-			<div class="wysiwyg">
-				<?php the_content(); ?>
-			</div>
-		</article>
+<div class="hero hero--internal">
+	<div class="hero__container">
+		<div class="hero__search"><?php mg_get_template_part('partials', 'form-search'); ?></div>
+	</div>
+</div>
 
-		<?php mg_get_sidebar(); ?>
+<div class="content wysiwyg">
+	<header class="page-header">
+		<h1 class="page-header__title"><?php the_title(); ?></h1>
+	</header>
+
+	<div class="wysiwyg">
+		<?php the_content(); ?>
 	</div>
 </div>
