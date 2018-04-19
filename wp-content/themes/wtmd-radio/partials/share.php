0d8bffb4321e4fd65756f9a76e2c49f1c8af15e1
Stephen Morton <smorton@mindgrub.com>
2018-04-18 22:26:04 -0400

    Tweet text change

--- wp-content/themes/wtmd-radio/partials/share.php
+++ wp-content/themes/wtmd-radio/partials/share.php
@@ -1,4 +1,4 @@
 <div class="share">
     <button class="share__button" data-share-channel="facebook" data-title="<?php the_title(); ?>" data-url="<?php the_permalink(); ?>" data-caption="WTMD"><i class="share__icon fa fa-facebook"></i>Share</button>
-    <button class="share__button" data-share-channel="twitter" data-text="<?php the_title(); ?>" data-url="<?php the_permalink(); ?>" data-via="WTMD"><i class="share__icon fa fa-twitter"></i>Tweet</button>
+    <button class="share__button" data-share-channel="twitter" data-text="I discovered <?php the_title(); ?> on @WTMD! Find more Baltimore-area bands and artists at Baltimore Music Mission, and by tuning into 89.7 WTMD." data-url="<?php the_permalink(); ?>"><i class="share__icon fa fa-twitter"></i>Tweet</button>
 </div>
